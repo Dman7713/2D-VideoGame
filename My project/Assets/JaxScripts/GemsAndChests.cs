932491367e6fb922ca2ff0eb5489a03323f3effd
Dman7713 <PeytonPrivratsky@gmail.com>
2024-11-06 10:11:09 -0800

    update

--- My project/Assets/JaxScripts/GemsAndChests.cs
+++ My project/Assets/JaxScripts/GemsAndChests.cs
@@ -5,20 +5,12 @@ using UnityEngine.UI;
 
 public class GemsAndChests : MonoBehaviour
 {
-
-
-    [SerializeField]
-    Image gem1;
-    [SerializeField]
-    Image gem2;
-    [SerializeField]
-    Image gem3;
-    [SerializeField]
-    Image gem4;
-    [SerializeField]
-    Canvas win;
-    [SerializeField]
-    Canvas HUD;
+    [SerializeField] Image gem1;
+    [SerializeField] Image gem2;
+    [SerializeField] Image gem3;
+    [SerializeField] Image gem4;
+    [SerializeField] Canvas win;
+    [SerializeField] Canvas HUD;
 
     // Start is called before the first frame update
     void Start()
@@ -28,13 +20,12 @@ public class GemsAndChests : MonoBehaviour
         gem3.GetComponent<Image>().enabled = false;
         gem4.GetComponent<Image>().enabled = false;
         win.GetComponent<Canvas>().enabled = false;
-
     }
 
     // Update is called once per frame
     void Update()
     {
-        Win(); 
+        Win();
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -42,30 +33,32 @@ public class GemsAndChests : MonoBehaviour
         if (collision.gameObject.tag == "Chest1")
         {
             gem1.GetComponent<Image>().enabled = true;
+            Destroy(collision.gameObject); // Destroy the chest after picking up gem1
         }
-        if (collision.gameObject.tag == "Chest2")
+        else if (collision.gameObject.tag == "Chest2")
         {
             gem2.GetComponent<Image>().enabled = true;
+            Destroy(collision.gameObject); // Destroy the chest after picking up gem2
         }
-        if (collision.gameObject.tag == "Chest3")
+        else if (collision.gameObject.tag == "Chest3")
         {
             gem3.GetComponent<Image>().enabled = true;
+            Destroy(collision.gameObject); // Destroy the chest after picking up gem3
         }
-        if (collision.gameObject.tag == "Chest4")
+        else if (collision.gameObject.tag == "Chest4")
         {
             gem4.GetComponent<Image>().enabled = true;
+            Destroy(collision.gameObject); // Destroy the chest after picking up gem4
         }
     }
+
     public void Win()
     {
         if (gem1.GetComponent<Image>().enabled == true && gem2.GetComponent<Image>().enabled == true && gem3.GetComponent<Image>().enabled == true && gem4.GetComponent<Image>().enabled == true)
         {
             HUD.GetComponent<Canvas>().enabled = false;
-           
             win.GetComponent<Canvas>().enabled = true;
             Time.timeScale = 0;
         }
-
-
     }
 }
